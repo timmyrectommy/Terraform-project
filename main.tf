8b0c81a24f7d9a3c9b24ce6bd922ff4910bb486a
timmyrectommy <timmyrectommy@yahoo.com>
2023-02-04 19:22:39 +0000

    terraform commit

---
+++ main.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:64e3df51cff2e8ffba6e5b460e5950165b82679b65f42eb36f03503fb591cd75
+size 3345
---
+++ provider.tf
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:36809e67ddb17578aebc919533d556432297225de0cb674ad3921eeac05f0780
+size 138
